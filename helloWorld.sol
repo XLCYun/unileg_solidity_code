1b287e63c5eaa2d774c7556fea3f4e5529feb4b9
XLCYun <xlcyun@foxmail.com>
2024-02-29 14:17:30 +0800

    feat: hello world

--- helloWorld.sol
+++ helloWorld.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
-/// @title 委托投票
-contract Ballot {
-    function HelloWorld() public pure returns (string memory) {
+/// @title Hellow World 示例
+contract HelloWorld {
+    function echo() public pure returns (string memory) {
         return "Hello World!";
     }
 }
