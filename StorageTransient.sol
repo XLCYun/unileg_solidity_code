d1ba2d2ff8d595e966cff0e1f867da9df9035285
XLCYun <xlcyun@foxmail.com>
2024-03-02 15:02:50 +0800

    fix: remove comment

--- StorageTransient.sol
+++ StorageTransient.sol
@@ -10,8 +10,6 @@ contract Storage {
             tstore(0, 1)
         }
         _;
-        // Unlocks the guard, making the pattern composable.
-        // After the function exits, it can be called again, even in the same transaction.
         assembly {
             tstore(0, 0)
         }
