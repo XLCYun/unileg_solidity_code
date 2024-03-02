51d6a80894825c0c55868d068c9d7683ecf92eff
XLCYun <xlcyun@foxmail.com>
2024-03-02 21:02:27 +0800

    feat: ValueTypeInteger

--- ValueTypeBoolean.sol
+++ ValueTypeBoolean.sol
@@ -6,14 +6,14 @@ contract ValueTypeBoolean {
     bool True = true;
     bool False = false;
 
-    // ! 操作
+    // ! 取反操作
     assert(!False); // 断言成功
     assert(!True);  // 断言失败
 
     // && 与操作
     assert(True && False); // 断言失败
 
-    // !! 或操作
+    // || 或操作
     assert(True || False); // 断言成功
 
     // == 比较相等
---
+++ ValueTypeInteger.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.24;
+
+contract ValueTypeInteger {
+  function div() public pure returns (int, fixed) {
+    int128 a = 2;
+    int128 b = 1;
+    fixed c = fixed(b) / fixed(a); // 显式转换为定点小数，可以
+    fixed c = b / a;               // 不可以
+  }
+}
