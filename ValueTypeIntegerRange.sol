e53b8be7b45772a85719784283a1c144f345eb41
XLCYun <xlcyun@foxmail.com>
2024-03-02 21:07:14 +0800

    fix: range

--- ValueTypeIntegerRange.sol
+++ ValueTypeIntegerRange.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 contract ValueTypeIntegerRange {
-  function div() public pure returns (int, int) {
+  function range() public pure returns (int, int) {
     return (type(int).min, type(int).max);
   }
 }
