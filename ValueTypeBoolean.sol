49f988137c5561333dac9673de070eaeaaac0250
XLCYun <xlcyun@foxmail.com>
2024-03-02 15:57:19 +0800

    fix: ValueTypeBoolean

--- ValueTypeBoolean.sol
+++ ValueTypeBoolean.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.24;
 
-contract ValueType {
+contract ValueTypeBoolean {
   function boolType() private pure {
     bool True = true;
     bool False = false;
