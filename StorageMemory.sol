c6c812151cc373f15aa24ab0fa2d8a871ff959fc
XLCYun <xlcyun@foxmail.com>
2024-03-02 13:40:12 +0800

    feat: add storage memory sol

---
+++ StorageMemory.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Counter {
+    uint number;
+    uint[] realG;
+
+    function computeSum(uint[] memory increments, uint len) private pure returns (uint sum) {
+        uint i;
+        for(i = 0; i < len; i++)
+          sum += increments[i];
+    }
+}
