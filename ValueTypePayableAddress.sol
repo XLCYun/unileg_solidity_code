437d27a53a7739d36a95770a64d20caacd7a9b28
XLCYun <xlcyun@foxmail.com>
2024-03-04 01:12:53 +0800

    refactor: format

--- ValueTypePayableAddress.sol
+++ ValueTypePayableAddress.sol
@@ -3,8 +3,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract ValueTypePaybleAddress {
     function sendCoin() public {
-      uint a = 0x111122223333444455556666777788889999AAAABBBBCCCCDDDDEEEEFFFFCCCC;
-      address payable addr = payable(address(uint160(a)));
-      addr.transfer(1 ether);
+        address payable addr = payable(
+            0x111122223333444455556666777788889999aAaa
+        );
+        addr.transfer(1 ether);
     }
 }
