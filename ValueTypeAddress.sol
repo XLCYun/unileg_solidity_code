f04f3c00dcb3721f78dc72ff4e1efdd0cd765719
XLCYun <xlcyun@foxmail.com>
2024-03-03 20:50:11 +0800

    fix: covert

--- ValueTypeAddress.sol
+++ ValueTypeAddress.sol
@@ -2,15 +2,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract ValueTypeAddress {
-    function addressConvert() public pure returns (address, address, address, address) {
+    function addressConvert() public pure returns (address, address) {
       uint a = 0x111122223333444455556666777788889999AAAABBBBCCCCDDDDEEEEFFFFCCCC;
       bytes32 b = bytes32(0x111122223333444455556666777788889999AAAABBBBCCCCDDDDEEEEFFFFCCCC);
 
       return (
-        address(uint160(a)), 
-        address(bytes20(b)), 
-        address(uint160(bytes20(b))),
-        address(bytes20(uint160(a)))
+        address(uint160(a)), // 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc 
+        address(bytes20(b)) // 0x111122223333444455556666777788889999aAaa
       );
     }
 }
