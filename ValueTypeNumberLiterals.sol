37ead0e903096714e8ab5e4db6c3941cacac9493
XLCYun <xlcyun@foxmail.com>
2024-03-04 16:37:25 +0800

    chore

--- ValueTypeNumberLiterals.sol
+++ ValueTypeNumberLiterals.sol
@@ -2,12 +2,11 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract ValueTypeNumberLiterals {
-    function num() public pure returns (int, int, int, int) {
+    function num() public pure returns (int, int, int) {
       return (
-        0x01,
-        1.5 * 2,
-        3.14e10,
-        123_456
+        0x01,     // 16 进制
+        3.14e10,  // 科学计数法
+        123_456   // 下划线分隔符
       );
     }
 }
