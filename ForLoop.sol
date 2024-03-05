8cb2cb57ee08442fc64d6a0fd0bd26b92744f829
XLCYun <xlcyun@foxmail.com>
2024-03-05 16:35:12 +0800

    ForLoop

--- ForLoop.sol
+++ ForLoop.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
-contract DoWhileLoop {
+contract ForLoop {
     function sum(uint n) public pure returns (uint) {
       uint res = 0;
       // 计算 1+2+...+n
-      do {
-        res += n;
-        n = n - 1;
-      } while( n > 0);
+      for(uint i = 0; i < n; i++) {
+        res = res + i;
+      }
       return res;
     }
 }
