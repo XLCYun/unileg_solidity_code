baa3c8b1770238004365df8552fbf07839c46672
XLCYun <xlcyun@foxmail.com>
2024-03-05 14:35:56 +0800

    ComplexTypeMapping

--- ComplexTypeMapping.sol
+++ ComplexTypeMapping.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 contract ComplexTypeMapping {
     mapping(string => int) scores;
     string[] names;
@@ -13,7 +15,7 @@ contract ComplexTypeMapping {
     function getScoresTable() public view returns (string memory) {
       string memory res = "";
       for(uint i = 0; i < names.length; i++) {
-        res = string.concat(res, names[i], string(abi.encode(scores[names[i]])), "\n");
+        res = string.concat(res, names[i], Strings.toString(uint(scores[names[i]])), "\n");
       }
       return res;
     }
