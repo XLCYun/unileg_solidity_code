b62d3b99a59adb703f896a5955aae67ab816f7ee
XLCYun <xlcyun@foxmail.com>
2024-03-07 18:24:17 +0800

    Contract addr

--- ValueTypeContractAddr.sol
+++ ValueTypeContractAddr.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract A {}
+contract A {
+    string public name;
+}
 
 contract B {
     string public name;
@@ -10,10 +12,14 @@ contract B {
 contract ValueTypeContract {
     function test() public returns (string memory) {
         A a = new A();
-        address aAddr = address(a);
+        address aAddr = address(a); // 合约转地址
 
-        // 强制转换为 B 地址
-        B b = B(aAddr);
-        return b.name();  // 将触发错误
+        A aRef = A(aAddr); // 地址转合约
+        string memory result = aRef.name(); // 正常调用
+
+        
+        B b = B(aAddr);  // 将合约 A 的地址强制转换为 B
+        result = b.name(); // 将触发错误
+        return result;
     }
 }
