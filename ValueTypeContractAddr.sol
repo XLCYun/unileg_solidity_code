efd0dc5cbb885822fe25bdbd07d7e702c93430d2
XLCYun <xlcyun@foxmail.com>
2024-03-07 18:21:49 +0800

    Contract addr

--- ValueTypeContractAddr.sol
+++ ValueTypeContractAddr.sol
@@ -3,10 +3,17 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract A {}
 
+contract B {
+    string public name;
+}
+
 contract ValueTypeContract {
-    function test() public returns (address) {
-      A a = new A();
-      address addr = address(a);
-      return addr;
+    function test() public returns (string memory) {
+        A a = new A();
+        address aAddr = address(a);
+
+        // 强制转换为 B 地址
+        B b = B(aAddr);
+        return b.name();  // 将触发错误
     }
 }
