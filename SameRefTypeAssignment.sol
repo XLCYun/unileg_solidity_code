ac4094d9532d9f00ed06b90d5327cf912388dfaa
XLCYun <xlcyun@foxmail.com>
2024-03-08 17:53:49 +0800

    ContractFactory

--- SameRefTypeAssignment.sol
+++ SameRefTypeAssignment.sol
@@ -2,18 +2,19 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract SameRefTypeAssignment {
+    int256[] storageA;
 
-  function fn(int[] calldata calldataA, int[] memory memoryA, int[] storage storageA) public {
-    int[] calldata calldataB;
-    // 合法，calldataB 和 calldataA 现在指向 calldata 中的同一片数据，未发生数组拷贝
-    calldataB = calldataA;
+    function fn(int256[] calldata calldataA, int256[] memory memoryA) public view {
+        int256[] calldata calldataB;
+        // 合法，calldataB 和 calldataA 现在指向 calldata 中的同一片数据，未发生数组拷贝
+        calldataB = calldataA;
 
-    int[] memory memoryB;
-    // 合法，memoryB 和 memoryA 现在指向 memory 中的同一片数据，未发生数组拷贝
-    memoryB = memoryA;
+        int256[] memory memoryB;
+        // 合法，memoryB 和 memoryA 现在指向 memory 中的同一片数据，未发生数组拷贝
+        memoryB = memoryA;
 
-    int[] storage storageB;
-    // 合法，storageB 和 storageA 现在指向 memory 中的同一片数据，未发生数组拷贝
-    storageB = storageA;
-  }
+        int256[] storage storageB;
+        // 合法，storageB 和 storageA 现在指向 memory 中的同一片数据，未发生数组拷贝
+        storageB = storageA;
+    }
 }
