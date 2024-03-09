cf5418ecd9cd12cd5777965770fa8f46e7ffa9fa
XLCYun <xlcyun@foxmail.com>
2024-03-09 16:10:01 +0800

    ContractFactory

--- FunctionMultipleModifier.sol
+++ FunctionMultipleModifier.sol
@@ -5,14 +5,18 @@ contract FunctionMultipleModifier {
     int256 public a;
 
     modifier runTwice() {
-        // 连着使用两个函数代码占位符，导致函数代码被执行两次
+        // 在本例子，此处函数体为 function addOne() public runTriple { a++; }
         _;
+        // 在本例子，此处函数体为 function addOne() public runTriple { a++; }
         _;
     }
 
     modifier runTriple() {
+        // 在本例中，此处函数体为 function addOne() public { a++; }
         _;
+        // 在本例中，此处函数体为 function addOne() public { a++; }
         _;
+        // 在本例中，此处函数体为 function addOne() public { a++; }
         _;
     }
 
