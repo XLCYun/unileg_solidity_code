5e39791ca45410c0ab2bb998d82239752338497f
XLCYun <xlcyun@foxmail.com>
2024-03-10 17:14:06 +0800

    ContractFactory

--- Constants.sol
+++ Constants.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.7.1 <0.9.0;
 
 // 文件级别常量
-uint constant kg = 1 * 1000;
+uint constant KG = 1 * 1000;
 
 contract Constants {
     // 合约内定义常量
-    uint constant ton = 1 * 1000 * kg;
+    uint constant TON = 1 * 1000 * KG;
 
     function tenTons() public pure returns (uint) {
-        return 1 * ton;
+        return 1 * TON;
     }
 }
