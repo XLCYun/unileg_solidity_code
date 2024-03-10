a4c506f69b825359fb1a28dfafb1e3c53038ca7a
XLCYun <xlcyun@foxmail.com>
2024-03-11 00:32:45 +0800

    "Greeting"

--- Greeting.sol
+++ Greeting.sol
@@ -12,3 +12,10 @@ abstract contract Greeting {
         return "Bye";
     }
 }
+
+// 英文版具体合约，可以被实例化
+contract EnglishGreeting is Greeting {
+  // 内部未重新实现 sayHi, sayBye 两个虚函数
+  // 但仍然是合法的，此时将使用虚函数的默认实现
+}
+
