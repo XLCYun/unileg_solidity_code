9f3dea506068d6e9bc802fb4144552bec25ab774
XLCYun <xlcyun@foxmail.com>
2024-03-12 00:07:02 +0800

    "LibraryInternalThis"

---
+++ LibraryInternalThis.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.1 <0.9.0;
+
+library Math {
+    // 库中的内部函数
+    function max() internal view returns (address) {
+        // 返回 this 的地址，在本例中，由于 max 是内部函数
+        // 因此 this 即是合约实例，即 this 地址等于合约的部署地址
+        return address(this);
+    }
+}
+
+contract LibraryInternal {
+    function test() public view returns (address) {
+        return Math.max();
+    }
+}
