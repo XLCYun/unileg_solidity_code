393972a7f6cfacef3278239141dbd1a23d449505
XLCYun <xlcyun@foxmail.com>
2024-03-12 19:28:47 +0800

    "usingFor"

--- usingFor.sol
+++ usingFor.sol
@@ -5,16 +5,20 @@ library L {
     function isEven(int a) internal pure returns (bool) {
         return a % 2 == 0;
     }
+
+    function isOdd(int a) internal pure returns (bool) {
+        return a % 2 != 0;
+    }
 }
 
 // 将库 L 中的所有非 private 函数附加到 int 类型
 using L for int;
 // 或者，只指定 L.isEven 附加到 int 类型
-using {L.isEven} for int;
+using {L.isEven, L.isOdd} for int;
 
 contract C {
-    function test() public pure returns (bool) {
+    function test() public pure returns (bool, bool) {
         int a = 3;
-        return a.isEven();
+        return (a.isEven(), a.isOdd());
     }
 }
