38c2b99e7e1d5ec26ac928822a52b35f74d1d6d6
XLCYun <xlcyun@foxmail.com>
2024-03-12 21:30:59 +0800

    "SetOperatorOverload"

--- Set2.sol
+++ Set2.sol
@@ -3,10 +3,9 @@ pragma solidity >=0.7.1 <0.9.0;
 
 /// @dev 数组实现的集合
 struct Set {
-    int[] elements;
+    int[100] elements;
+    uint length;
 }
-
-using SetOp for Set global;
 
 /// @dev 数组实现的集合操作
 library SetOp {
@@ -14,8 +13,8 @@ library SetOp {
     /// @param self 集合结构体
     /// @param v 要查找的元素
     /// @return bool true 表示存在，false 表示不存在
-    function has(Set storage self, int v) internal view returns (bool) {
-        for (uint i = 0; i < self.elements.length; i++)
+    function has(Set memory self, int v) internal pure returns (bool) {
+        for (uint i = 0; i < self.length; i++)
             if (self.elements[i] == v) return true;
         return false;
     }
@@ -24,9 +23,9 @@ library SetOp {
     /// @param self 集合结构体
     /// @param v 要插入的元素
     /// @return bool true 表示插入成功，false 表示元素已存在
-    function insert(Set storage self, int v) internal returns (bool) {
+    function insert(Set memory self, int v) internal pure returns (bool) {
         if (has(self, v)) return false;
-        self.elements.push(v);
+        self.elements[self.length++] = v;
         return true;
     }
 
@@ -34,12 +33,12 @@ library SetOp {
     /// @param self 集合结构体
     /// @param v 要移除的元素
     /// @return bool true 表示移除成功，false 表示元素不存在
-    function remove(Set storage self, int v) internal returns (bool) {
+    function remove(Set memory self, int v) internal pure returns (bool) {
         for (uint i = 0; i < self.elements.length; i++)
             if (self.elements[i] == v) {
                 for (i++; i < self.elements.length; i++)
                     self.elements[i - 1] = self.elements[i];
-                self.elements.pop();
+                self.length--;
                 return true;
             }
         return false;
@@ -48,7 +47,7 @@ library SetOp {
     /// @notice 并集操作
     /// @param self 集合结构体，并集结果仍存回此集合
     /// @param unionSet 将此集合元素并入 self 集合
-    function union(Set storage self, Set storage unionSet) internal {
+    function union(Set memory self, Set memory unionSet) pure internal {
         for (uint i = 0; i < unionSet.elements.length; i++)
             insert(self, unionSet.elements[i]);
     }
@@ -56,16 +55,8 @@ library SetOp {
     /// @notice 交集操作
     /// @param self 集合结构体，交集结果仍存回此集合
     /// @param diffSet 将此集合元素与 self 取交集，不修改此集合
-    function diff(Set storage self, Set storage diffSet) internal {
+    function diff(Set memory self, Set memory diffSet) internal pure {
         for (uint i = 0; i < diffSet.elements.length; i++)
             remove(self, diffSet.elements[i]);
-    }
-
-    /// @notice 获取所有元素
-    /// @param self 集合结构体，交集结果仍存回此集合
-    function getElements(
-        Set storage self
-    ) internal view returns (int[] storage) {
-        return self.elements;
     }
 }
