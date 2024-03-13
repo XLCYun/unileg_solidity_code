e30713ab7677a31878f347eaa859cdf0ed7d9d8a
XLCYun <xlcyun@foxmail.com>
2024-03-13 12:01:48 +0800

    "EventAnonymous"

---
+++ EventAnonymous.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.1 <0.9.0;
+
+contract Event {
+    // 定义一个事件 SomebodyCall，接受两个参数，指定为索引
+    event SomebodyCall(string indexed name, uint256 indexed time) anonymous;
+
+    function call(string calldata name) public {
+        // 发送事件 SomebodyCall
+        emit SomebodyCall(name, block.timestamp);
+    }
+}
