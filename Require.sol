d811682bd9b9159115c744c020f93648cf46b4e6
XLCYun <xlcyun@foxmail.com>
2024-03-19 16:41:52 +0800

    "Require"

--- Require.sol
+++ Require.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.7.1 <0.9.0;
 contract Require {
     address owner;
 
-    function Destroy() public {
+    function Destroy(address payable addr) public {
         require(owner == msg.sender, "Not owner");
-        selfdestruct();
+        selfdestruct(addr);
     }
 }
