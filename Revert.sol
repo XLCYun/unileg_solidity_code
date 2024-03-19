a04b3573e1e41ceb8125b97c48e947e1c670048a
XLCYun <xlcyun@foxmail.com>
2024-03-19 16:19:13 +0800

    "Revert"

--- Revert.sol
+++ Revert.sol
@@ -2,12 +2,16 @@
 pragma solidity >=0.7.1 <0.9.0;
 
 contract Revert {
-    function revert1() public pure {
+    int count;
+
+    function revert1() public {
+        count++;
         // 直接抛出错误，不符带任何数据
         revert();
     }
 
-    function revert2() public pure {
+    function revert2() public {
+        count++;
         // 抛出错误，带一个字符串揭示错误原因
         revert("Not owner");
     }
@@ -15,7 +19,8 @@ contract Revert {
     // 一个自定义错误
     error NotOwner(address from);
 
-    function revert3() public view {
+    function revert3() public {
+        count++;
         // 抛出自定义错误
         revert NotOwner(msg.sender);
     }
