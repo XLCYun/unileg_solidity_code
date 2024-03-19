a9475a3f9864e707fcfa0a26aae6f869fce65a6f
XLCYun <xlcyun@foxmail.com>
2024-03-19 16:43:07 +0800

    "Require"

--- Require.sol
+++ Require.sol
@@ -5,6 +5,7 @@ contract Require {
     address owner;
 
     function Destroy(address payable addr) public {
+        // 要求 owner 等于 msg.sender，否则抛出错误 Error("Not owner")
         require(owner == msg.sender, "Not owner");
         selfdestruct(addr);
     }
